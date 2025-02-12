a4c179f52bff04d2b237bf7b525fff5307cb9997
romumupointparis <romubuntu@gmail.com>
2025-02-12 22:26:54 +0100

    styled component on compiler

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@ const nextConfig = {
     env:{
         NEXT_PUBLIC_POCKETBASE_UR:'https://server.romubuntu.dev',
         SOCKET_URL:'http://server.romubuntu.dev:3001'
-    }
+    },
+    compiler: {
+        styledComponents: true,
+      },
 };
 
 export default nextConfig;
