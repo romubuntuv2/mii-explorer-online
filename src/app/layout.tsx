00dad1eb49d3b55f579377ca8fbf6ed1ec27898e
romumupointparis <romubuntu@gmail.com>
2025-02-24 20:58:27 +0100

    test google app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from 'next/font/local'
 import {Ubuntu} from 'next/font/google'
 import { Metadata } from "next";
+import Script from 'next/script';
 
 
 export const metadata: Metadata = {
@@ -26,6 +27,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-6Q1WVRMDBK"></Script>
+      <Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-6Q1WVRMDBK');
+      </Script>
+      </head>
       <body className={`${ubutuntu.variable} ${continuum.variable}`} style={{margin:0, cursor:'none'}}>
         {children}
       </body>
