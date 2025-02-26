d5557671c09e2c363da8c285d93f8cb4aae054fc
romumupointparis <romubuntu@gmail.com>
2025-02-26 21:24:38 +0100

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 import {Ubuntu} from 'next/font/google'
 import { Metadata } from "next";
-import Script from 'next/script';
+import {Analytics} from '@vercel/analytics/next'
 
 
 export const metadata: Metadata = {
@@ -27,19 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-6Q1WVRMDBK"></Script>
-      <Script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-6Q1WVRMDBK');
-      </Script>
-      </head>
-      <body className={`${ubutuntu.variable} ${continuum.variable}`} style={{margin:0, cursor:'none'}}>
-        {children}
-      </body>
+    <body className={`${ubutuntu.variable} ${continuum.variable}`} style={{margin:0, cursor:'none'}}>
+      {children}
+      <Analytics />
+    </body>
     </html>
   );
 }
